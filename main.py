0b9417d4ade79350421e2714e24239cf7ef23002
davidpazukshevchenko <useplay132010@gmail.com>
2023-11-04 10:25:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import random
+
+class Cat:
+    def __init__(self,name):
+        self.name = name
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+    def to_hunts(self):
+        print("Time to hunts")
+        self.progress += 0.12
+        self.gladness -= 7
+
+    def to_sleep(self):
+        print("I will sleep")
+        self.gladness += 3
+
+    def to_play_with_the_owner(self):
+        print("Time to play")
+        self.gladness += 5
+        self.progress -= 0.1
+
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Cast out...")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depression...")
+            self.alive = False
+        elif self.progress > 5:
+            print("It got too boring")
+            self.alive = False
+
+    def end_of_play(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress,  2)}")
+
+    def live(self, day):
+        day = "Day " + str(day) + " of " + self.name + " life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1, 3)
+
+        if live_cube == 1:
+            self.to_hunts()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_play_with_the_owner()
+
+        self.end_of_play()
+        self.is_alive()
+
+
+nick = Cat(name="Marty")
+
+for day in range(1, 366):
+    if nick.alive == False:
+        break
+
+    nick.live(day)
